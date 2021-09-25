e1bd3e57b8404a5e951f992efd735e3d446ee59c
THE-WARRIOR-CR7 <82370377+THE-WARRIOR-CR7@users.noreply.github.com>
2021-09-25 08:15:16 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function preload() {
+
+}
+function setup() {
+    canvas=createCanvas(375,375);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    posenet=ml5.poseNet(video,modelLoaded);
+posenet.on("pose",gotPoses);
+}
+function draw() {
+image(video, 0, 0, 375, 375);
+}
+function snapshot() {
+    save("mustache.png");
+}
+function modelLoaded() {
+    console.log("model is loaded");
+
+}
+function gotPoses(results) {
+if(results.length>0) {
+    console.log(results);
+    console.log("nose x "+results[0].pose.nose.x);
+} console.log("nose y "+results[0].pose.nose.y);
+}
